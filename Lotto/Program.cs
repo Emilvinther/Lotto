072f367533685a6e9d9f8b7b2f602bfd368b0709
Emil Vinther Lorentzen <emil380e@zbc.dk>
2022-03-22 15:10:44 +0100

    Add project files.

---
+++ Lotto/Program.cs
@@ -0,0 +1,39 @@
+﻿int [] winner = new int [7];
+int [] player = new int [7];
+Random random = new Random ();
+int winnernumbers = 0;
+
+for (int i = 0; i < winner.Length; i++)
+{
+    winner [i] = random.Next (1, 20);
+}
+
+for (int i = 0; i < player.Length; i++)
+{
+    Console.WriteLine("Vælg et nummer");
+    player[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+//for (int i = 0; i < player.Length; i++)
+//{
+
+//    Console.Write(player[i] + " ");
+//}
+
+for (int i = 0; i < player.Length; i++)
+{
+    for (int j = 0; j < winner.Length; j++)
+    {
+        if (player[i] == winner[j])
+        {
+            winnernumbers++;
+        }
+    }
+
+}
+
+Console.WriteLine ("Du har {0} rigtige", winnernumbers);
+
+
+Console.ReadKey ();
+
